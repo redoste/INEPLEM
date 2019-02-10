ed250443bfe2b1bb4680100754def45d412ca0e8
redoste <jub.csc@gmail.com>
2019-02-10 18:28:16 +0100

    Ajout d'un build script pour windows-service

---
+++ windows-service/build.bat
@@ -0,0 +1,26 @@
+@echo off
+if "%1" == "d" goto debug
+if "%1" == "r" goto release
+goto error
+
+:debug
+set cflags=
+set lflags=-o INEPLEM1.debug.exe
+goto build
+:release
+set cflags=-Os -s
+set lflags=-static -Os -s -o INEPLEM1.release.exe
+goto build
+
+:build
+mkdir obj 2>nul
+echo on
+g++ -std=c++17 main.cpp -c -o obj/main.o -Wall -Wextra %cflags%
+g++ -std=c++17 watchdog.cpp -c -o obj/watchdog.o -Wall -Wextra %cflags%
+g++ -std=c++17 obj/main.o obj/watchdog.o %lflags%
+@echo off
+goto end
+
+:error
+echo Invalid build target
+:end
